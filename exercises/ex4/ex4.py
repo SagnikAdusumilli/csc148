f94501489c74808146049c2ab4f31c69d975d0cd
SagnikAdusumilli <sagnik.adusumilli@gmail.com>
2016-10-28 12:54:37 -0400

    finished ex4

--- exercises/ex4/ex4.py
+++ exercises/ex4/ex4.py
@@ -18,7 +18,6 @@ It is divided into two parts:
 ##############################################################################
 
 
-# TODO: Implement this!
 def duplicate(nested_list):
     """Return a new nested list with all numbers in <nested_list> duplicated.
 
@@ -31,19 +30,26 @@ def duplicate(nested_list):
     @type nested_list: list | int
     @rtype: list
 
-    >>> duplicate(1)
-    [1, 1]
-    >>> duplicate([])
-    []
+
     >>> duplicate([1, 2])
     [1, 1, 2, 2]
     >>> duplicate([1, [2, 3]])  # NOT [1, 1, [2, 2, 3, 3], [2, 2, 3, 3]]
     [1, 1, [2, 2, 3, 3]]
     """
-    pass
+    lst = []
 
+    if isinstance(nested_list, int):
+        lst.extend([nested_list, nested_list])
+        return lst
+    else:
+        for item in nested_list:
+            if isinstance(item, int):
+                lst.extend(duplicate(item))
+            else:
+                lst.append(duplicate(item))
 
-# TODO: Implement this!
+        return lst
+
 def add_one(nested_list):
     """Add one to every number stored in <nested_list>.
 
@@ -67,7 +73,16 @@ def add_one(nested_list):
     >>> lst2
     [2, [3, 4], [[[6]]]]
     """
-    pass
+    if isinstance(nested_list, int):
+        return None
+
+    else:
+        for i in range(len(nested_list)):
+            if isinstance(nested_list[i], int):
+                nested_list[i] += 1
+            else:
+                add_one(nested_list[i])
+
 
 
 ##############################################################################
@@ -100,7 +115,15 @@ class Person:
         @type self: Person
         @rtype: int
         """
-        pass
+        if self.children == []:
+            return 0
+        else:
+            count = len(self.children)
+
+            for child in self.children:
+               count += child.count_descendants()
+
+            return count
 
 
 if __name__ == '__main__':
